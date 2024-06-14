1c5c732e107b7755656848353b8db92448369883
Golan Halley <ghalley42@gmail.com>
2024-06-14 12:45:12 -0600

    fixing merge conflict

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,3 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient('https://oxqgutjwfdolrcwlfqja.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94cWd1dGp3ZmRvbHJjd2xmcWphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc4NzU1MzgsImV4cCI6MjAzMzQ1MTUzOH0.d7FFrWQFccEG9VcsEAVBXFwNUsmO8j3j1VfsPYoSwXs');
+const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
