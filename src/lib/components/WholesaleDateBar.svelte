61b5ac4ca8f8cef77b8bbf131a0cbb178d16bdb0
Golan Halley <ghalley42@gmail.com>
2024-06-15 11:28:00 -0600

    Added dependencies and tightened logic for dates.
    Added shadcn date picker, popover, menubar. Cleaned up logic for date formatter and date filter.

---
+++ src/lib/components/WholesaleDateBar.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+import * as Menubar from '$lib/components/ui/menubar';
+import CalendarIcon from "lucide-svelte/icons/calendar";
+import { DateFormatter, type DateValue, getLocalTimeZone } from "@internationalized/date";
+import { cn } from "$lib/utils.js";
+import { Button } from "$lib/components/ui/button/index.js";
+import { Calendar } from "$lib/components/ui/calendar/index.js";
+import * as Popover from "$lib/components/ui/popover/index.js";
+
+</script>
+
+
--- src/routes/wholesale-tracker/+page.svelte
+++ src/routes/wholesale-tracker/+page.svelte
@@ -6,7 +6,14 @@
     export let data;
 
     const formatDate = (date) => {
-        return [date.getFullYear(), date.getMonth() + 1, date.getDate().toString().padStart(2, '0')].toString().replaceAll(',','-')
+        let month = date.getMonth() + 1;
+        month = month.toString()
+        return [date.getFullYear(), month.padStart(2, '0'), date.getDate().toString().padStart(2, '0')].toString().replaceAll(',','-')
+    }    
+
+    const filterByDateRange = (array, start, end) => {
+        console.log(start, end);
+        return array.filter(e => new Date(e.date) >= new Date(formatDate(start)) && new Date(e.date) <= new Date(formatDate(end)))
     }
 
     const customerList = Array.from(data.customerData).map(e => e.name);
@@ -20,7 +27,7 @@
     <Table.Caption>Wholesale Customer Sales by Period</Table.Caption>
     <Table.Header>
         <Table.Row>
-            <Table.Head rowspan="2"><strong>CUSTOMER</strong></Table.Head>
+            <Table.Head rowspan="2">CUSTOMER</Table.Head>
             {#each productCategories as cat}
             <Table.Head colspan={productRows.filter(e => e.category == cat).length}>{cat.toUpperCase()}</Table.Head>
             {/each}
@@ -32,6 +39,8 @@
         </Table.Row>
     </Table.Header>
     <Table.Body>
-        <WholesaleRow customer={"Alley Cat"} orders={orders} itemList={Array.from(productRows).map(e => e.code)}/>
+        <!-- {#each customerList as customer}
+        <WholesaleRow customer={customer} orders={orders.filter(e)} itemList={Array.from(productRows).map(e => e.code)}/>
+        {/each} -->
     </Table.Body>
 </Table.Root>
