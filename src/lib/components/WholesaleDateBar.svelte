c8795b87e24c62ae93ef59bc241a67aef46bbb65
Golan Halley <ghalley42@gmail.com>
2024-06-23 14:51:53 -0600

    Added logic to wholsale-tracker page
    Worked on filtering logic for customers based on having orders between dates.

--- src/lib/components/WholesaleDateBar.svelte
+++ src/lib/components/WholesaleDateBar.svelte
@@ -7,6 +7,31 @@ import { Button } from "$lib/components/ui/button/index.js";
 import { Calendar } from "$lib/components/ui/calendar/index.js";
 import * as Popover from "$lib/components/ui/popover/index.js";
 
+const df = new DateFormatter("en-US", {
+    dateStyle: "long",
+  });
+ 
+  let value: DateValue | undefined = undefined;
+
 </script>
 
-
+<div>
+    <Popover.Root openFocus>
+        <Popover.Trigger asChild let:builder>
+          <Button
+            variant="outline"
+            class={cn(
+              "w-[280px] justify-start text-left font-normal",
+              !value && "text-muted-foreground"
+            )}
+            builders={[builder]}
+          >
+            <CalendarIcon class="mr-2 h-4 w-4" />
+            {value ? df.format(value.toDate(getLocalTimeZone())) : "Select a date"}
+          </Button>
+        </Popover.Trigger>
+        <Popover.Content class="w-auto p-0">
+          <Calendar bind:value initialFocus />
+        </Popover.Content>
+      </Popover.Root>
+</div>
--- src/routes/wholesale-tracker/+page.svelte
+++ src/routes/wholesale-tracker/+page.svelte
@@ -1,7 +1,9 @@
-<script>
+<script lang='ts'>
     import * as Table from '$lib/components/ui/table'
     import { productRows, productCategories } from '$lib/commonVariables.js';
     import WholesaleRow from '$lib/components/WholesaleRow.svelte';
+	import WholesaleDateBar from '@/components/WholesaleDateBar.svelte';
+	import { CableIcon } from 'lucide-svelte';
 
     export let data;
 
@@ -9,20 +11,25 @@
         let month = date.getMonth() + 1;
         month = month.toString()
         return [date.getFullYear(), month.padStart(2, '0'), date.getDate().toString().padStart(2, '0')].toString().replaceAll(',','-')
-    }    
+    }
+
+    const setStartDate = (date) => new Date(date.getFullYear(), date.getMonth(), date.getDate() - 7);
 
     const filterByDateRange = (array, start, end) => {
-        console.log(start, end);
         return array.filter(e => new Date(e.date) >= new Date(formatDate(start)) && new Date(e.date) <= new Date(formatDate(end)))
     }
 
+    const getTotals = (customer) => filterByDateRange(orders, startDate, endDate).filter(e => e.customer_name == customer).reduce((total, currentValue) => total + currentValue.qty, 0)
+
     const customerList = Array.from(data.customerData).map(e => e.name);
     const orders = Array.from(data.orderData);
-    let startDate = new Date('2024-5-10');
-    let endDate = new Date('2024-5-20');
+    let endDate = new Date(2024, 4, 15);
+    let startDate = setStartDate(endDate);
+    
 
 
 </script>
+<WholesaleDateBar />
 <Table.Root>
     <Table.Caption>Wholesale Customer Sales by Period</Table.Caption>
     <Table.Header>
@@ -39,8 +46,16 @@
         </Table.Row>
     </Table.Header>
     <Table.Body>
-        <!-- {#each customerList as customer}
-        <WholesaleRow customer={customer} orders={orders.filter(e)} itemList={Array.from(productRows).map(e => e.code)}/>
-        {/each} -->
+        {#each customerList as customer}
+        {#if getTotals(customer) != 0}
+        <WholesaleRow customer={customer} orders={filterByDateRange(orders, startDate, endDate)} itemList={Array.from(productRows).map(e => e.code)}/>
+        {/if}
+        {/each}
     </Table.Body>
 </Table.Root>
+<h2>Customers Who Didn't Order:</h2>
+{#each customerList as customer}
+{#if getTotals(customer) == 0}
+<p>{customer}</p>
+{/if}
+{/each}
